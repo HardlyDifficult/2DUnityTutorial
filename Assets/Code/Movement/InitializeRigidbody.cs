01cc03a7105a47dd4a864e853c0a0d60bb5c2c77
hardlydifficultllc <hardlydifficultllc@gmail.com>
2017-08-10 12:10:28 -0700

    InitializeRigidbody

---
+++ Assets/Code/Movement/InitializeRigidbody.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Gives the rigidbody initial momentum.
+/// </summary>
+[RequireComponent(typeof(Rigidbody2D))]
+public class InitializeRigidbody : MonoBehaviour
+{
+  [SerializeField]
+  Vector2 startingVelocity = new Vector2(3, 0);
+
+  [SerializeField]
+  float startingAngularVelocity = -500;
+
+  protected void Start()
+  {
+    Rigidbody2D myBody = GetComponent<Rigidbody2D>();
+    Debug.Assert(myBody != null);
+
+    // Set initial momentum
+    myBody.velocity = startingVelocity;
+    myBody.angularVelocity = startingAngularVelocity;
+  }
+}
