0e83100eb10e4e017b749d16c0c2cc1d27c109c3
hardlydifficultllc <hardlydifficultllc@gmail.com>
2017-08-10 12:55:36 -0700

    comments

--- Assets/Code/Death/DestroyOutOfBounds.cs
+++ Assets/Code/Death/DestroyOutOfBounds.cs
@@ -1,10 +1,17 @@
 ﻿using UnityEngine;
 
+/// <summary>
+/// Destroy the GameObject anytime it falls off screen.
+/// </summary>
 public class DestroyOutOfBounds : MonoBehaviour
 {
+  /// <summary>
+  /// 2 below the lowest point the camera can see.
+  /// Hardcoded for simplicity.
+  /// </summary>
   const float outOfBoundsYPosition = -12;
 
-  protected void Update()
+  protected void FixedUpdate()
   {
     if(transform.position.y < outOfBoundsYPosition)
     {
