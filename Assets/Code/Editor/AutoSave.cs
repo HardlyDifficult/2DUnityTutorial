fff5b41029e384838f233eac4c31fec976950bc5
hardlydifficultllc <hardlydifficultllc@gmail.com>
2017-08-09 17:08:06 -0700

    AutoSave

---
+++ Assets/Code/Editor/AutoSave.cs
@@ -0,0 +1,28 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+
+/// <summary>
+/// An editor script which automatically saves every time you
+/// hit play.
+/// </summary>
+[InitializeOnLoad]
+public class AutoSave
+{
+  static AutoSave()
+  {
+    EditorApplication.playmodeStateChanged
+      += OnPlaymodeStateChanged;
+  }
+
+  /// <summary>
+  /// Called on enter/exit play mode and pause.
+  /// </summary>
+  static void OnPlaymodeStateChanged()
+  {
+    // Skip saving if in play mode, e.g. during pause
+    if(EditorApplication.isPlaying == false)
+    {
+      EditorSceneManager.SaveOpenScenes();
+    }
+  }
+}
