983c71cb99dcbe8fb2ef7da580a45151b05948be
hardlydifficultllc <hardlydifficultllc@gmail.com>
2017-08-10 11:44:33 -0700

    Spawner

---
+++ Assets/Code/Controllers/Spawner.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using UnityEngine;
+
+/// <summary>
+/// Periodically spawns a GameObject at this object's position.
+/// </summary>
+public class Spawner : MonoBehaviour
+{
+  [SerializeField]
+  GameObject thingToSpawn;
+  
+  [SerializeField]
+  float minTimeBetweenSpawns = .5f;
+
+  [SerializeField]
+  float maxTimeBetweenSpawns = 10;
+
+  protected void Start()
+  {
+    Debug.Assert(thingToSpawn != null);
+    Debug.Assert(minTimeBetweenSpawns >= 0);
+    Debug.Assert(maxTimeBetweenSpawns > 0);
+    Debug.Assert(maxTimeBetweenSpawns >= minTimeBetweenSpawns);
+
+    StartCoroutine(SpawnEnemiesCoroutine());
+  }
+
+  IEnumerator SpawnEnemiesCoroutine()
+  {
+    while(true)
+    {
+      Instantiate(
+        thingToSpawn,
+        transform.position,
+        Quaternion.identity);
+
+      // Sleep before the next spawn
+      float sleepTime = UnityEngine.Random.Range(
+        minTimeBetweenSpawns,
+        maxTimeBetweenSpawns);
+      yield return new WaitForSeconds(sleepTime);
+    }
+  }
+}
