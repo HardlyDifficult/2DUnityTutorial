e110c259663d93802575037633532317df86834f
hardlydifficultllc <hardlydifficultllc@gmail.com>
2017-08-10 12:49:25 -0700

    DestroyOutOfBounds

---
+++ Assets/Code/Death/DestroyOutOfBounds.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+  const float outOfBoundsYPosition = -12;
+
+  protected void Update()
+  {
+    if(transform.position.y < outOfBoundsYPosition)
+    {
+      Destroy(gameObject);
+    }
+  }
+}
