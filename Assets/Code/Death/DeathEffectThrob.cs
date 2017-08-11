974b88016875d2a6e8d6eca01976f80987da93e4
hardlydifficultllc <hardlydifficultllc@gmail.com>
2017-08-11 15:55:22 -0700

    Animate the character's death.

---
+++ Assets/Code/Death/DeathEffectThrob.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// When death effects are triggered on this GameObject, this
+/// causes the GameObject to scale up and down until it's gone.
+/// </summary>
+public class DeathEffectThrob : DeathEffect
+{
+  [SerializeField]
+  float lengthOfEffectInSeconds = 1;
+
+  /// <summary>
+  /// How many times to scale up and then down
+  /// in the lengthOfEffectInSeconds.
+  /// </summary>
+  [SerializeField]
+  int numberOfPulses = 5;
+
+  Vector3 originalScale;
+
+  protected void Awake()
+  {
+    Debug.Assert(lengthOfEffectInSeconds > 0);
+    Debug.Assert(numberOfPulses > 0);
+
+    originalScale = transform.localScale;
+
+    Debug.Assert(originalScale.sqrMagnitude > 0);
+  }
+
+  public override float PlayDeathEffects()
+  {
+    StartCoroutine(ThrobToDeath());
+
+    return lengthOfEffectInSeconds;
+  }
+
+  IEnumerator ThrobToDeath()
+  {
+    float timePerPulse
+      = lengthOfEffectInSeconds / numberOfPulses;
+
+    float timeRun = 0;
+    while(timeRun < lengthOfEffectInSeconds)
+    {
+      float percentComplete
+        = timeRun / lengthOfEffectInSeconds;
+      float sinValue
+        = Mathf.Sin(Mathf.PI * timeRun / timePerPulse);
+      float pulse = .5f + Mathf.Abs(sinValue);
+      float scale = (1 - percentComplete) * pulse;
+      gameObject.transform.localScale
+        = originalScale * scale; 
+
+      yield return null;
+      timeRun += Time.deltaTime;
+    }
+  }
+}
