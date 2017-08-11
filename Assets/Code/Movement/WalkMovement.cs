df86bb2f138d64bf81f676ef2bcd8a617ae5bc48
hardlydifficultllc <hardlydifficultllc@gmail.com>
2017-08-10 20:45:52 -0700

    WalkMovement

---
+++ Assets/Code/Movement/WalkMovement.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Sets the rigidbody X velocity each FixedUpdate,
+/// allowing entities to walk given a desiredWalkDirection.
+/// </summary>
+[RequireComponent(typeof(Rigidbody2D))]
+public class WalkMovement : MonoBehaviour
+{
+  /// <summary>
+  /// Set by another component to request movement.
+  /// Should be -1 (left) -> 1 (right)
+  /// </summary>
+  [Tooltip("Leave at 0 in the Inspector")]
+  [Range(-1, 1)]
+  public float desiredWalkDirection;
+
+  [SerializeField]
+  float walkSpeed = 100;
+
+  Rigidbody2D myBody;
+
+  protected void Awake()
+  {
+    Debug.Assert(desiredWalkDirection == 0,
+      "desiredWalkDirection should be 0 in the Inspector");
+
+    myBody = GetComponent<Rigidbody2D>();
+
+    Debug.Assert(myBody != null);
+  }
+
+  protected void FixedUpdate()
+  {
+    float desiredXVelocity
+      = desiredWalkDirection
+        * walkSpeed
+        * Time.fixedDeltaTime;
+
+    // Set the X, even if we are not walking 
+    // (to control horizontal momentum),
+    // preserve the current Y (e.g. for gravity).
+    myBody.velocity = new Vector2(
+      desiredXVelocity,
+      myBody.velocity.y);
+  }
+}
