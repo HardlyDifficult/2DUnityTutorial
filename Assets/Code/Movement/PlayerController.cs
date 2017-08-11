80e0e0adb4551e4f8b8c1af675a3fde506743817
hardlydifficultllc <hardlydifficultllc@gmail.com>
2017-08-10 20:50:23 -0700

    PlayerController

---
+++ Assets/Code/Movement/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Reads input in order to drive movement.
+/// </summary>
+[RequireComponent(typeof(WalkMovement))]
+public class PlayerController : MonoBehaviour
+{
+  WalkMovement walkMovement;
+
+  protected void Awake()
+  {
+    walkMovement = GetComponent<WalkMovement>();
+
+    Debug.Assert(walkMovement != null);
+  }
+
+  protected void FixedUpdate()
+  {
+    walkMovement.desiredWalkDirection
+      = Input.GetAxis("Horizontal");
+  }
+}
