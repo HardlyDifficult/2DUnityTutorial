b1e88c79aa8299be817858cb8612cd6a887de10f
hardlydifficultllc <hardlydifficultllc@gmail.com>
2017-08-11 18:17:59 -0700

    Clear level

---
+++ Assets/Code/Death/DestroyWhenPlayerDies.cs
@@ -0,0 +1,10 @@
+﻿/// <summary>
+/// When the player dies, this GameObject will be destroyed.
+/// </summary>
+public class DestroyWhenPlayerDies : PlayerDeathMonoBehaviour
+{
+  public override void OnPlayerDeath()
+  {
+    Destroy(gameObject);
+  }
+}
