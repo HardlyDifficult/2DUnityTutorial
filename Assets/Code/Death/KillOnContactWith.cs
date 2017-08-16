20f8e96280a778b441e08e65180e92e6ecef3470
hardlydifficultllc <hardlydifficultllc@gmail.com>
2017-08-16 16:15:07 -0700

    Kill the character.

---
+++ Assets/Code/Death/KillOnContactWith.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Trigger death effects and then destroy any GameObject
+/// we come in contact with if it matches the LayerMask.
+/// </summary>
+[RequireComponent(typeof(Collider2D))]
+public class KillOnContactWith : MonoBehaviour
+{
+  [SerializeField]
+  LayerMask layersToKill;
+
+  /// <summary>
+  /// Added to allow enable/disable in the Inspector.
+  /// </summary>
+  protected void Start() { }
+
+  protected void OnCollisionEnter2D(
+    Collision2D collision)
+  {
+    TryToKill(collision.gameObject);
+  }
+
+  protected void OnTriggerEnter2D(
+    Collider2D collision)
+  {
+    TryToKill(collision.gameObject);
+  }
+
+  void TryToKill(
+    GameObject gameObjectWeHit) 
+  { 
+    if(enabled == false)
+    {
+      return;
+    }
+    
+    if(layersToKill.Includes(gameObjectWeHit.layer))
+    {
+      DeathEffectManager.PlayDeathEffectsThenDestroy(
+        gameObjectWeHit);
+    }
+  }
+}
---
+++ Assets/Code/Utils/LayerMaskExtensions.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Extension methods for Unity's LayerMask.
+/// </summary>
+public static class LayerMaskExtensions
+{
+  /// <summary>
+  /// Check if a layer is included in a given layer mask.
+  /// </summary>
+  public static bool Includes(
+    this LayerMask mask,
+    int layer)
+  {
+    return (mask.value & (1 << layer)) > 0;
+  }
+}
