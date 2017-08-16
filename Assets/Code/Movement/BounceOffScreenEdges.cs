67dd4898f8913c9d4953e044cb26a8f56e9943e8
hardlydifficultllc <hardlydifficultllc@gmail.com>
2017-08-16 16:55:47 -0700

    Bounce.

---
+++ Assets/Code/Movement/BounceOffScreenEdges.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// When KeepOnScreen detects this GameObject went off screen,
+/// this component will switch the WalkMovement direction.
+/// </summary>
+[RequireComponent(typeof(KeepOnScreen))]
+[RequireComponent(typeof(WalkMovement))]
+public class BounceOffScreenEdges : MonoBehaviour
+{
+  WalkMovement walkMovement;
+
+  protected void Awake()
+  {
+    walkMovement = GetComponent<WalkMovement>();
+
+    Debug.Assert(walkMovement != null);
+  }
+
+  protected void Start()
+  {
+    KeepOnScreen keepOnScreen = GetComponent<KeepOnScreen>();
+    Debug.Assert(keepOnScreen != null);
+
+    keepOnScreen.onAttemptToLeaveScreen
+      += KeepOnScreen_onAttemptToLeaveScreen;
+  }
+
+  void KeepOnScreen_onAttemptToLeaveScreen()
+  {
+    // If on the right side, walk left. And vice versa.
+    walkMovement.desiredWalkDirection =
+      transform.position.x > 0 ? -1 : 1;
+  }
+}
