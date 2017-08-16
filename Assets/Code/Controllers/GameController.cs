d4dc9d3845a1613d5137dbb9bce6b8b2e829f8ef
hardlydifficultllc <hardlydifficultllc@gmail.com>
2017-08-16 16:38:09 -0700

    GameController

---
+++ Assets/Code/Controllers/GameController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using UnityEngine;
+
+/// <summary>
+/// A singleton / DontDestroyOnLoad used to persist information
+/// across scenes, such as points.
+/// </summary>
+public class GameController : MonoBehaviour
+{
+  public static GameController instance;
+
+  public event Action onLifeCountChange;
+
+  [SerializeField]
+  int _lifeCount = 3;
+  public int lifeCount
+  {
+    get
+    {
+      return _lifeCount;
+    }
+    set
+    {
+      _lifeCount = value;
+      if(onLifeCountChange != null)
+      {
+        onLifeCountChange();
+      }
+    }
+  }
+
+  public int points;
+
+  public Bounds screenBounds
+  {
+    get; private set;
+  }
+
+  int originalLifeCount;
+
+  protected void Awake()
+  {
+    Debug.Assert(lifeCount > 0);
+    Debug.Assert(points == 0);
+
+    // If there is already a GameController, destroy the extra.
+    if(instance != null)
+    {
+      Destroy(gameObject);
+      return;
+    }
+
+    instance = this;
+    DontDestroyOnLoad(gameObject);
+
+    originalLifeCount = lifeCount;
+
+    // Ensure screenBounds has a value when the game begins
+    CalcScreenSize();
+  }
+
+  protected void Update()
+  {
+    // Update each frame in case the camera changed: which 
+    // should not happen except for during scene changes.
+    CalcScreenSize();
+  }
+
+  void CalcScreenSize()
+  {
+    Vector2 screenSize = new Vector2(
+          (float)Screen.width / Screen.height,
+          1);
+    screenSize *= Camera.main.orthographicSize * 2;
+    screenBounds = new Bounds(
+      (Vector2)Camera.main.transform.position,
+      screenSize);
+  }
+}
---
+++ Assets/Code/Death/DeathEffectDecrementLives.cs
@@ -0,0 +1,14 @@
+﻿/// <summary>
+/// When death effects are triggered on this GameObject, this
+/// reduces the GameControllers lifeCount by one.
+/// </summary>
+public class DeathEffectDecrementLives : DeathEffect
+{
+  public override float PlayDeathEffects()
+  {
+    GameController.instance.lifeCount--;
+
+    // GameObject may be destroyed now
+    return 0;
+  }
+}
