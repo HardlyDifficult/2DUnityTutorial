6e7cc0724b782fc347ddf06932de95286ed445be
hardlydifficultllc <hardlydifficultllc@gmail.com>
2017-08-16 16:25:01 -0700

    SuicideIn

---
+++ Assets/Code/Death/SuicideIn.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Begins death effects and then destroys this GameObject
+/// after timeTillDeath seconds.
+/// </summary>
+public class SuicideIn : MonoBehaviour
+{
+  [SerializeField]
+  float timeTillDeath = 5;
+
+  protected void Start()
+  {
+    Debug.Assert(timeTillDeath >= 0);
+
+    StartCoroutine(CountdownToDeath());
+  }
+
+  IEnumerator CountdownToDeath()
+  {
+    yield return new WaitForSeconds(timeTillDeath);
+    DeathEffectManager.PlayDeathEffectsThenDestroy(gameObject);
+  }
+}
