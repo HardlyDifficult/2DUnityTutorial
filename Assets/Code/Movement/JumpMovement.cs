3e5237691fb7249fb870a123d6e6c2fa2c04b1d6
hardlydifficultllc <hardlydifficultllc@gmail.com>
2017-08-16 13:48:34 -0700

    Jump

---
+++ Assets/Code/Movement/JumpMovement.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Adds a vertical impulse force when a jump is requested.
+/// </summary>
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(AudioSource))]
+public class JumpMovement : MonoBehaviour
+{
+  /// <summary>
+  /// Set by another component to request a jump.
+  /// </summary>
+  public bool jumpRequested;
+
+  [SerializeField]
+  AudioClip jumpSound;
+
+  [SerializeField]
+  float jumpSpeed = 7f;
+
+  Rigidbody2D myBody;
+
+  AudioSource audioSource;
+
+  protected void Awake()
+  {
+    Debug.Assert(jumpSound != null);
+    Debug.Assert(jumpSpeed > 0);
+
+    myBody = GetComponent<Rigidbody2D>();
+    audioSource = GetComponent<AudioSource>();
+
+    Debug.Assert(myBody != null);
+    Debug.Assert(audioSource != null);
+  }
+
+  protected void FixedUpdate()
+  {
+    if(jumpRequested)
+    {
+      // Jump
+      myBody.AddForce(
+          new Vector2(0, jumpSpeed),
+          ForceMode2D.Impulse);
+
+      audioSource.PlayOneShot(jumpSound);
+    }
+
+    jumpRequested = false;
+  }
+}
--- Assets/Code/Movement/PlayerController.cs
+++ Assets/Code/Movement/PlayerController.cs
@@ -4,20 +4,33 @@
 /// Reads input in order to drive movement.
 /// </summary>
 [RequireComponent(typeof(WalkMovement))]
+[RequireComponent(typeof(JumpMovement))]
 public class PlayerController : MonoBehaviour
 {
   WalkMovement walkMovement;
 
+  JumpMovement jumpMovement;
+
   protected void Awake()
   {
     walkMovement = GetComponent<WalkMovement>();
+    jumpMovement = GetComponent<JumpMovement>();
 
     Debug.Assert(walkMovement != null);
+    Debug.Assert(jumpMovement != null);
   }
 
   protected void FixedUpdate()
   {
     walkMovement.desiredWalkDirection
       = Input.GetAxis("Horizontal");
+  }
+
+  protected void Update()
+  {
+    if(Input.GetButtonDown("Jump"))
+    {
+      jumpMovement.jumpRequested = true;
+    }
   }
 }
