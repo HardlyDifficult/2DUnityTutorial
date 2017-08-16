45c6a5e8adc9d3263f1509d0b1be69b06a499ed0
hardlydifficultllc <hardlydifficultllc@gmail.com>
2017-08-16 16:03:59 -0700

    Pattern.

---
+++ Assets/Code/Death/DeathEffect.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Base class for logic which is executed when an entity dies.
+/// </summary>
+[RequireComponent(typeof(DeathEffectManager))]
+public abstract class DeathEffect : MonoBehaviour
+{
+  /// <summary>
+  /// Do not call directly.  
+  /// Use DeathEffectManager instead.
+  /// </summary>
+  /// <returns>
+  /// How long before the GameObject may be destroyed.
+  /// </returns>
+  public abstract float PlayDeathEffects();
+}
