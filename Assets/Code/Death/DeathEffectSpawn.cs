c3ed1659735fbda01d683dab7f12bac17f4a1db6
hardlydifficultllc <hardlydifficultllc@gmail.com>
2017-08-16 16:29:17 -0700

    Spawn

---
+++ Assets/Code/Death/DeathEffectSpawn.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// When death effects are triggered on this GameObject, this
+/// spawns another GameObject.
+/// </summary>
+[RequireComponent(typeof(Collider2D))]
+public class DeathEffectSpawn : DeathEffect
+{
+  [SerializeField]
+  GameObject gameObjectToSpawnOnDeath;
+
+  public override float PlayDeathEffects()
+  {
+    Debug.Assert(gameObjectToSpawnOnDeath != null);
+
+    Collider2D collider = GetComponent<Collider2D>();
+    Debug.Assert(collider != null);
+
+    Instantiate(
+      gameObjectToSpawnOnDeath,
+      collider.bounds.center,
+      Quaternion.identity);
+
+    // GameObject may be destroyed now
+    return 0;
+  }
+}
