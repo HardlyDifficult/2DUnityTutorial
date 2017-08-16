115001087bde3695dbe021f364904b6f96f665dd
hardlydifficultllc <hardlydifficultllc@gmail.com>
2017-08-16 16:43:39 -0700

    On screen

---
+++ Assets/Code/Movement/KeepOnScreen.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+
+/// <summary>
+/// Prevents a GameObject from leaving the visible area
+/// of the world.
+/// </summary>
+[RequireComponent(typeof(Rigidbody2D))]
+public class KeepOnScreen : MonoBehaviour
+{
+  Rigidbody2D myBody;
+
+  public event Action onAttemptToLeaveScreen;
+
+  protected void Awake()
+  {
+    myBody = GetComponent<Rigidbody2D>();
+
+    Debug.Assert(myBody != null);
+  }
+
+  protected void FixedUpdate()
+  {
+    Bounds screenBounds = GameController.instance.screenBounds;
+    if(screenBounds.Contains(transform.position) == false)
+    {
+      // Move to the closest on-screen location
+      transform.position =
+        screenBounds.ClosestPoint(transform.position);
+      if(onAttemptToLeaveScreen != null)
+      {
+        onAttemptToLeaveScreen();
+      }
+    }
+  }
+}
