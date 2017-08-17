90a8559679a22be3471b90a42911ecd61269c686
hardlydifficultllc <hardlydifficultllc@gmail.com>
2017-08-17 15:26:20 -0700

    RandomClimbController

---
+++ Assets/Code/Movement/RandomClimbController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using UnityEngine;
+
+/// <summary>
+/// Uses the LadderMovement component to randomly climb
+/// ladders up or down.
+/// </summary>
+[RequireComponent(typeof(LadderMovement))]
+public class RandomClimbController : MonoBehaviour
+{
+  [SerializeField]
+  float oddsOfClimbingLadderUp = .9f;
+
+  [SerializeField]
+  float oddsOfClimbingLadderDown = .1f;
+
+  [SerializeField]
+  float minTimeBetweenReconsideringDirection = 1;
+
+  [SerializeField]
+  float maxTimeBetweenReconsideringDirection = 10;
+
+  LadderMovement ladderMovement;
+
+  protected void Awake()
+  {
+    Debug.Assert(oddsOfClimbingLadderUp >= 0);
+    Debug.Assert(oddsOfClimbingLadderDown >= 0);
+    Debug.Assert(oddsOfClimbingLadderUp > 0 
+      || oddsOfClimbingLadderDown > 0);
+    Debug.Assert(minTimeBetweenReconsideringDirection >= 0);
+    Debug.Assert(maxTimeBetweenReconsideringDirection > 0);
+    Debug.Assert(minTimeBetweenReconsideringDirection
+      < maxTimeBetweenReconsideringDirection);
+
+    ladderMovement = GetComponent<LadderMovement>();
+
+    Debug.Assert(ladderMovement != null);
+  }
+
+  protected void Start()
+  {
+    StartCoroutine(Wander());
+  }
+
+  IEnumerator Wander()
+  {
+    while(true)
+    {
+      SelectARandomClimbDirection();
+      float timeToSleep = UnityEngine.Random.Range(
+        minTimeBetweenReconsideringDirection,
+        maxTimeBetweenReconsideringDirection);
+      yield return new WaitForSeconds(timeToSleep);
+    }
+  }
+
+  void SelectARandomClimbDirection()
+  {
+    if(ladderMovement.isOnLadder == false)
+    {
+      if(UnityEngine.Random.value <= oddsOfClimbingLadderUp)
+      {
+        ladderMovement.desiredClimbDirection = 1;
+      }
+      else if(UnityEngine.Random.value
+        <= oddsOfClimbingLadderDown)
+      {
+        ladderMovement.desiredClimbDirection = -1;
+      }
+      else
+      {
+        ladderMovement.desiredClimbDirection = 0;
+      }
+    }
+  }
+}
