f65cfb23030ef0b272275acaee8e03ead8630d30
hardlydifficultllc <hardlydifficultllc@gmail.com>
2017-08-16 17:00:18 -0700

    Respawn

---
+++ Assets/Code/Controllers/LevelController.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Coordinates level specific activities.
+/// </summary>
+public class LevelController : MonoBehaviour
+{
+  [SerializeField]
+  GameObject characterPrefab;
+
+  bool isGameOver;
+
+  protected void Start()
+  {
+    Debug.Assert(characterPrefab != null);
+    Debug.Assert(isGameOver == false);
+
+    GameController.instance.onLifeCountChange
+      += Instance_onLifeCounterChange;
+
+    StartLevel();
+  }
+
+  protected void OnDestroy()
+  {
+    GameController.instance.onLifeCountChange
+      -= Instance_onLifeCounterChange;
+  }
+
+  void Instance_onLifeCounterChange()
+  {
+    if(isGameOver)
+    {
+      return;
+    }
+
+    if(GameController.instance.lifeCount <= 0)
+    {
+      isGameOver = true;
+      YouLose();
+    }
+    else
+    {
+      StartLevel();
+    }
+  }
+
+  public void YouWin()
+  {
+    if(isGameOver == true)
+    {
+      return;
+    }
+    isGameOver = true;
+
+    // TODO
+  }
+
+  void StartLevel()
+  {
+    Instantiate(characterPrefab);
+  }
+
+  void YouLose()
+  {
+    // TODO
+  }
+}
