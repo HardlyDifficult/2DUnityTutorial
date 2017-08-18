895b914684ba36afb634745b1d0a957d61ae9162
hardlydifficultllc <hardlydifficultllc@gmail.com>
2017-08-18 11:38:50 -0700

    Hammer rotation fix

--- Assets/Code/Weapons/WeaponHolder.cs
+++ Assets/Code/Weapons/WeaponHolder.cs
@@ -3,17 +3,7 @@
 /// <summary>
 /// This component allows an entity to hold a weapon.
 /// </summary>
-[RequireComponent(typeof(TurnAround))]
 public class WeaponHolder : MonoBehaviour
 {
-  public TurnAround turnAround;
-
   public GameObject currentWeapon;
-
-  protected void Awake()
-  {
-    turnAround = GetComponent<TurnAround>();
-
-    Debug.Assert(turnAround != null);
-  }
 }
