fb9b65b2bb07809283b278fd13b17e7b15f7272e
hardlydifficultllc <hardlydifficultllc@gmail.com>
2017-08-17 19:18:32 -0700

    Create Hammer

---
+++ Assets/Code/Weapons/Weapon.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Added to a weapon to manage pickup and equip.
+/// </summary>
+public class Weapon : MonoBehaviour
+{
+  [SerializeField]
+  Vector2 positionWhenEquip = new Vector2(.214f, .17f);
+
+  [SerializeField]
+  Vector3 rotationWhenEquipInEuler = new Vector3(0, 0, -90);
+
+  [SerializeField]
+  MonoBehaviour[] componentListToEnableOnEquip;
+
+  WeaponHolder currentHolder;
+  
+  protected void OnDestroy()
+  {
+    if(currentHolder != null)
+    {
+      currentHolder.currentWeapon = null;
+    }
+  }
+
+  protected void OnTriggerEnter2D(
+    Collider2D collision)
+  {
+    WeaponHolder holder = collision.GetComponent<WeaponHolder>();
+    if(holder != null 
+      && currentHolder == null 
+      && holder.currentWeapon == null)
+    {
+      currentHolder = holder;
+      currentHolder.currentWeapon = gameObject;
+
+      transform.SetParent(currentHolder.transform);
+      transform.localPosition = positionWhenEquip;
+      transform.localRotation 
+        = Quaternion.Euler(rotationWhenEquipInEuler);
+
+      for(int i = 0; i < componentListToEnableOnEquip.Length; i++)
+      {
+        MonoBehaviour component = componentListToEnableOnEquip[i];
+        component.enabled = true;
+      }
+    }
+  }
+}
---
+++ Assets/Code/Weapons/WeaponHolder.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// This component allows an entity to hold a weapon.
+/// </summary>
+public class WeaponHolder : MonoBehaviour
+{
+  public GameObject currentWeapon;
+}
