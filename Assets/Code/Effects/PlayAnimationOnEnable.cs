6b940faaa23900fab9c19e8dc439a400647e38a1
hardlydifficultllc <hardlydifficultllc@gmail.com>
2017-08-18 14:56:33 -0700

    Start swinging.

---
+++ Assets/Code/Effects/PlayAnimationOnEnable.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// When this component is enabled, the selected animation
+/// is played.
+/// </summary>
+public class PlayAnimationOnEnable : MonoBehaviour
+{
+  [SerializeField]
+  string animationToPlay;
+
+  Animator animator;
+
+  protected void Awake()
+  {
+    animator = GetComponentInChildren<Animator>();
+  }
+
+  protected void OnEnable()
+  {
+    animator.Play(animationToPlay);
+  }
+}
