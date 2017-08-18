4fc45d04bc724bc05c83d8869c67df9c67f976c9
hardlydifficultllc <hardlydifficultllc@gmail.com>
2017-08-18 16:00:18 -0700

    Animation params

---
+++ Assets/Code/Animations/CharacterAnimatorController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Each frame, reports various stats to the animation 
+/// controller.
+/// </summary>
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(LadderMovement))]
+[RequireComponent(typeof(WeaponHolder))]
+public class CharacterAnimatorController : MonoBehaviour
+{
+  Animator animator;
+
+  Rigidbody2D myBody;
+
+  LadderMovement ladderMovement;
+
+  FloorDetector floorDetector;
+
+  WeaponHolder weaponHolder;
+
+  protected void Awake()
+  {
+    animator = GetComponentInChildren<Animator>();
+    myBody = GetComponent<Rigidbody2D>();
+    ladderMovement = GetComponent<LadderMovement>();
+    floorDetector = GetComponentInChildren<FloorDetector>();
+    weaponHolder = GetComponent<WeaponHolder>();
+
+    Debug.Assert(animator != null);
+    Debug.Assert(myBody != null);
+    Debug.Assert(ladderMovement != null);
+    Debug.Assert(floorDetector != null);
+    Debug.Assert(weaponHolder != null);
+  }
+
+  protected void LateUpdate() 
+  {
+    animator.SetFloat("Speed", myBody.velocity.magnitude);
+    animator.SetBool("isTouchingFloor", 
+      floorDetector.isTouchingFloor);
+    animator.SetBool("isClimbing", ladderMovement.isOnLadder);
+    animator.SetBool("hasWeapon", 
+      weaponHolder.currentWeapon != null);
+  }
+}
