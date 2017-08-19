5e0de82c3e2c1de0a22ce1d069ed33172b465aec
hardlydifficultllc <hardlydifficultllc@gmail.com>
2017-08-18 19:12:23 -0700

    Spawners

---
+++ Assets/Code/Animations/EnableComponentsOnTimelineEvent.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// When this script is triggerd by the TimelineEventPlayable
+/// a list of components will be enabled.
+/// </summary>
+public class EnableComponentsOnTimelineEvent : MonoBehaviour
+{
+  [SerializeField]
+  TimelineEventPlayable.EventType eventType;
+
+  [SerializeField]
+  Behaviour[] componentListToEnable;
+
+  public void OnEvent(
+    TimelineEventPlayable.EventType currentEventType)
+  {
+    if(currentEventType == eventType)
+    {
+      for(int i = 0; i < componentListToEnable.Length; i++)
+      {
+        Behaviour component = componentListToEnable[i];
+        component.enabled = true;
+      }
+    }
+  }
+}
---
+++ Assets/Code/Animations/TimelineEventPlayable.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+/// <summary>
+/// A playable to be used in a Timeline.  When initiated 
+/// it will call every EnableComponentsOnTimelineEvent
+/// in the scene with the select event type.
+/// </summary>
+public class TimelineEventPlayable : BasicPlayableBehaviour
+{
+  public enum EventType
+  {
+    AlmostAtStart, Start, End
+  }
+
+  /// <summary>
+  /// The event type to broadcast for this instance 
+  /// in the Timeline.
+  /// </summary>
+  [SerializeField]
+  EventType eventType;
+
+  public override void OnBehaviourPlay(
+    Playable playable,
+    FrameData info)
+  {
+    base.OnBehaviourPlay(playable, info);
+
+    EnableComponentsOnTimelineEvent[] componentList
+      = GameObject.FindObjectsOfType<EnableComponentsOnTimelineEvent>();
+
+    for(int i = 0; i < componentList.Length; i++)
+    {
+      EnableComponentsOnTimelineEvent component = componentList[i];
+      component.OnEvent(eventType);
+    }
+  }
+}
