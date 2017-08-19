024b5f0380ecf27343afa7034fac4d6814a2e308
hardlydifficultllc <hardlydifficultllc@gmail.com>
2017-08-19 16:08:42 -0700

    Transition scenes

---
+++ Assets/Code/Animations/ChangeScenePlayable.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.SceneManagement;
+using UnityEngine.Timeline;
+
+/// <summary>
+/// This will load a scene when it first appears in a Timeline.
+/// </summary>
+public class ChangeScenePlayable : BasicPlayableBehaviour
+{
+  [SerializeField]
+  string sceneNameToLoad;
+
+  public override void OnBehaviourPlay(
+    Playable playable,
+    FrameData info)
+  {
+    Debug.Assert(string.IsNullOrEmpty(sceneNameToLoad) == false);
+
+    base.OnBehaviourPlay(playable, info);
+
+    SceneManager.LoadScene(sceneNameToLoad);
+  }
+}
