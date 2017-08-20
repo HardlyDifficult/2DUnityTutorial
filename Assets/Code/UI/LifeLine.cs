0f16fa225a36cdc725c7fa9e83e0f62ccca44abf
hardlydifficultllc <hardlydifficultllc@gmail.com>
2017-08-20 10:37:20 -0700

    Lives

---
+++ Assets/Code/UI/LifeLine.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// This component will kill its GameObject when the 
+/// number of lives remaining drops below the selected value.
+/// </summary>
+public class LifeLine : PlayerDeathMonoBehaviour
+{
+  [SerializeField]
+  int lifeCount = 1;
+
+  public override void OnPlayerDeath()
+  {
+    if(GameController.instance.lifeCount < lifeCount)
+    {
+      DeathEffectManager.PlayDeathEffectsThenDestroy(gameObject);
+    }
+  }
+}
