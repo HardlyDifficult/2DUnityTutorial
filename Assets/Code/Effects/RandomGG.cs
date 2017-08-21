e7538b29b6386f42313c64a2f46acd0165795feb
hardlydifficultllc <hardlydifficultllc@gmail.com>
2017-08-20 19:21:40 -0700

    GG

---
+++ Assets/Code/Effects/RandomGG.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// This will randomize the scale, color, and position
+/// of a TextMesh.  To be used on the GG screen.
+/// </summary>
+public class RandomGG : MonoBehaviour
+{
+  [SerializeField]
+  float minScale = .05f;
+
+  [SerializeField]
+  float maxScale = .25f;
+
+  protected void OnEnable()
+  {
+    transform.localScale
+      = Vector3.one * UnityEngine.Random.Range(minScale, maxScale);
+
+    TextMesh text = GetComponent<TextMesh>();
+    text.color = UnityEngine.Random.ColorHSV();
+
+    Bounds screenBounds = GameController.instance.screenBounds;
+    transform.position = new Vector3(
+      UnityEngine.Random.Range(screenBounds.min.x, screenBounds.max.x),
+      screenBounds.max.y + 10,
+      0);
+  }
+}
---
+++ Assets/Code/UI/AnyKeyToLoadScene.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// When any key is pressed, the given scene is loaded.
+/// </summary>
+public class AnyKeyToLoadScene : MonoBehaviour
+{
+  [SerializeField]
+  string sceneName = "Menu";
+
+  protected void Update()
+  {
+    Debug.Assert(string.IsNullOrEmpty(sceneName) == false);
+
+    if(Input.anyKeyDown)
+    {
+      SceneManager.LoadScene(sceneName);
+    }
+  }
+}
