4c5471474759658e8bbe68429abf3c7c2e4d878b
hardlydifficultllc <hardlydifficultllc@gmail.com>
2017-08-20 18:34:32 -0700

    Breakaway

---
+++ Assets/Code/Effects/UnfreezeAndDisablePlatformers.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// When this component is enabled, constraints are removed
+/// from the rigidbody and the platform effector, if any, 
+/// is disabled.
+/// </summary>
+public class UnfreezeAndDisablePlatformers : MonoBehaviour
+{
+  protected void OnEnable()
+  {
+    Rigidbody2D myBody = GetComponent<Rigidbody2D>();
+    myBody.constraints = RigidbodyConstraints2D.None;
+
+    PlatformEffector2D effector
+      = GetComponent<PlatformEffector2D>();
+    if(effector != null)
+    {
+      effector.enabled = false;
+    }
+  }
+}
